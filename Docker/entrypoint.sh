e01bb7aee8efef223f0cb1ce2c9363709c4670fc
Ermias Mekonnen <ermiasmekonnen28@gmail.com>
2023-11-28 23:46:06 +0300

    idk

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -14,17 +14,11 @@ fi
 role=${CONTAINER_ROLE:-app}
 
 if [ "$role" = "app" ]; then
-    php artisan migrate
     php artisan key:generate
     php artisan cache:clear
     php artisan config:clear
     php artisan route:clear
     php artisan serve --port=$PORT --host=0.0.0.0 --env=.env
     exec docker-php-entrypoint "$@"
-elif [ "$role" = "queue" ]; then
-    echo "Running the queue ... "
-    php /var/www/artisan queue:work --verbose --tries=3 --timeout=180
-elif [ "$role" = "websocket" ]; then
-    echo "Running the websocket server ... "
-    php artisan websockets:serve
+
 fi
